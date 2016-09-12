e121e5e610bf87cefe6b31f8a0950b7195594586
felire <feliress13@gmail.com>
2016-09-12 16:02:06 -0300

    script

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -1,168 +1,247 @@
+CREATE PROCEDURE CrearTablas
+AS
 CREATE TABLE Roles (
-	Rol_id INT IDENTITY(1,1) PRIMARY KEY,
-	Rol_nombre VARCHAR (20) NOT NULL,
-	Rol_activo BIT NOT NULL); -- El BIT es BOOLEANO, 0 es false y 1 es true.
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Nombre VARCHAR (20) NOT NULL,
+	Esta_activo BIT NOT NULL); -- El BIT es BOOLEANO, 0 es false y 1 es true.
 
 CREATE TABLE Funciones (
-	Funcion_id INT IDENTITY(1,1) PRIMARY KEY,
-	Fun_nombre VARCHAR(50) NOT NULL);
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Nombre VARCHAR(50) NOT NULL);
 
 CREATE TABLE Funciones_Roles(
 	Rol_id INT,
 	Funcion_id INT,
 	PRIMARY KEY(Rol_id, Funcion_id),
-	FOREIGN KEY(Rol_id) REFERENCES Roles (Rol_id),
-	FOREIGN KEY(Funcion_id) REFERENCES Funciones (Funcion_id));
+	FOREIGN KEY(Rol_id) REFERENCES Roles (Id),
+	FOREIGN KEY(Funcion_id) REFERENCES Funciones (Id));
 
 CREATE TABLE Usuarios(
-	Usuario_id VARCHAR(50) PRIMARY KEY, --Nombre usuario
-	Usuario_password VARCHAR(150) NOT NULL,
-	Usuario_nombre VARCHAR(255),
-	Usuario_intentos INT DEFAULT 0,
-	Usuario_habilitado BIT NOT NULL DEFAULT 1); --1 es habilitado, 0 no.
+	Nombre_usuario VARCHAR(50) PRIMARY KEY, --Nombre usuario
+	Password_usuario CHAR(32) NOT NULL,
+	Nombre VARCHAR(255),
+	Intentos_fallidos INT DEFAULT 0,
+	Habilitado BIT NOT NULL DEFAULT 1); --1 es habilitado, 0 no.
 
 CREATE TABLE Roles_Usuario(
 	Rol_id INT,
 	Usuario_id VARCHAR(50),
 	PRIMARY KEY(Rol_id,Usuario_id),
-	FOREIGN KEY(Rol_id) REFERENCES Roles (Rol_id),
-	FOREIGN KEY(Usuario_id) REFERENCES Usuarios (Usuario_id)); 
+	FOREIGN KEY(Rol_id) REFERENCES Roles (Id),
+	FOREIGN KEY(Usuario_id) REFERENCES Usuarios (Nombre_usuario)); 
 
 CREATE TABLE Planes(	--Todos valores de tabla maestra
-	Plan_codigo numeric(18,0) PRIMARY KEY,
-	Plan_descripcion VARCHAR(255),
-	Plan_precio_bono_consulta numeric(18,0),
-	Plan_precio_bono_farmacia numeric(18,0));
+	Codigo numeric(18,0) PRIMARY KEY,
+	Descripcion VARCHAR(255),
+	Precio_bono_consulta numeric(18,0),
+	Precio_bono_farmacia numeric(18,0));
 
 CREATE TABLE Grupos_Familiares(
-	Grupo_id INT IDENTITY(1,1) PRIMARY KEY,
-	Grupo_plan numeric(18,0),
-	FOREIGN KEY (Grupo_plan) REFERENCES Planes (Plan_codigo));
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Plan_Grupo numeric(18,0),
+	FOREIGN KEY (Plan_Grupo) REFERENCES Planes (Codigo));
 
 CREATE TABLE Afiliados(
-	Afiliado_id INT IDENTITY(1,1) PRIMARY KEY,
-	Afiliado_grupo INT,
-	Afiliado_numero INT, --Dentro del grupo familiar
-	Afiliado_nombre VARCHAR(255) NOT NULL,
-	Afiliado_apellido VARCHAR(255) NOT NULL,
-	Afiliado_tipo VARCHAR(15) NOT NULL CHECK (Afiliado_tipo IN ('DNI', 'LD', 'LC', 'CI')),
-	Afiliado_nroDoc numeric(18,0) NOT NULL,
-	Afiliado_direccion VARCHAR(255) NOT NULL,
-	Afiliado_telefono numeric(18,0) NULL,
-	Afiliado_mail VARCHAR(255) NOT NULL,
-	Afiliado_fec_nac DATETIME NOT NULL,
-	Afiliado_sexo CHAR CHECK (Afiliado_sexo IN ('M', 'F')),
-	Afiliado_estado_civil VARCHAR(20) CHECK (Afiliado_estado_civil IN ('Soltero/a', 'Casado/a', 'Viudo/a', 'Concubinato', 'Divorciado/a')),
-	Afiliado_familiares_a_cargo INT,
-	Afiliado_activo BIT, -- 1 activo, 0 desactivado
-	FOREIGN KEY (Afiliado_grupo) REFERENCES Grupos_Familiares(Grupo_id));
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Numero_de_Grupo INT,
+	Numero_en_el_grupo INT, --Dentro del grupo familiar
+	Nombre VARCHAR(255) NOT NULL,
+	Apellido VARCHAR(255) NOT NULL,
+	Tipo_doc VARCHAR(15) NOT NULL CHECK (Tipo_doc IN ('DNI', 'LD', 'LC', 'CI')),
+	Numero_doc numeric(18,0) NOT NULL,
+	Direccion VARCHAR(255) NOT NULL,
+	Telefono numeric(18,0) NULL,
+	Mail VARCHAR(255) NOT NULL,
+	Fecha_nacimiento DATETIME NOT NULL,
+	Sexo CHAR CHECK (Sexo IN ('M', 'F')),
+	Estado_civil VARCHAR(20) CHECK (Estado_civil IN ('Soltero/a', 'Casado/a', 'Viudo/a', 'Concubinato', 'Divorciado/a')),
+	Familiares_a_cargo INT,
+	Esta_activo BIT, -- 1 activo, 0 desactivado
+	Nombre_usuario VARCHAR(50),
+	FOREIGN KEY (Nombre_usuario) REFERENCES Usuarios (Nombre_usuario),
+	FOREIGN KEY (Numero_de_Grupo) REFERENCES Grupos_Familiares(Id));
 
 CREATE TABLE LogsCambioAfiliados(
-	Cambio_id INT IDENTITY (1,1) PRIMARY KEY,
-	Cambio_afiliado INT,
-	Cambio_fecha DATETIME,
-	Cambio_descripcion VARCHAR(255)
-	FOREIGN KEY(Cambio_afiliado) REFERENCES Afiliados (Afiliado_id));
+	Id INT IDENTITY (1,1) PRIMARY KEY,
+	Afiliado INT,
+	Fecha DATETIME,
+	Descripcion VARCHAR(255)
+	FOREIGN KEY(Afiliado) REFERENCES Afiliados (Id));
 
 
 CREATE TABLE Transacciones(
-	Transaccion_id INT IDENTITY(1,1) PRIMARY KEY,
-	Tansaccion_cantidad INT NOT NULL,
-	Transaccion_precio INT NOT NULL,
-	Transaccion_fecha DATETIME NOT NULL,
-	Transaccion_afiliado INT,
-	FOREIGN KEY (Transaccion_afiliado) REFERENCES Afiliados (Afiliado_id));
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Cantidad INT NOT NULL,
+	Precio INT NOT NULL,
+	Fecha DATETIME NOT NULL,
+	Afiliado INT,
+	FOREIGN KEY (Afiliado) REFERENCES Afiliados (Id));
 
 CREATE TABLE Profesionales(
-	Profesional_id INT IDENTITY(1,1) PRIMARY KEY,
-	Profesional_nombre VARCHAR(255) NOT NULL,
-	Profesional_apellido VARCHAR(255) NOT NULL,
-	Profesional_tipo VARCHAR(15) NOT NULL CHECK (Profesional_tipo IN ('DNI', 'LD', 'LC', 'CI')),
-	Profesional_nroDoc numeric(18,0) NOT NULL,
-	Profesional_direccion VARCHAR(255) NOT NULL,
-	Profesional_telefono numeric(18,0) NULL,
-	Profesional_mail VARCHAR(255) NOT NULL,
-	Profesional_fec_nac DATETIME NOT NULL,
-	Profesional_sexo CHAR CHECK (Profesional_sexo IN ('M', 'F')),
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Nombre VARCHAR(255) NOT NULL,
+	Apellido VARCHAR(255) NOT NULL,
+	Tipo_doc VARCHAR(15) NOT NULL CHECK (Tipo_doc IN ('DNI', 'LD', 'LC', 'CI')),
+	Numero_doc numeric(18,0) NOT NULL,
+	Direccion VARCHAR(255) NOT NULL,
+	Telefono numeric(18,0) NULL,
+	Mail VARCHAR(255) NOT NULL,
+	Fecha_nacimiento DATETIME NOT NULL,
+	Sexo CHAR CHECK (Sexo IN ('M', 'F')),
+	Usuario_id VARCHAR(50),
+	FOREIGN KEY (Usuario_id) REFERENCES Usuarios (Nombre_usuario),
 	Profesional_matricula VARCHAR (50));
 
 CREATE TABLE Tipo_Especialidad(
-	Tipo_codigo numeric(18,0) PRIMARY KEY,
-	Tipo_descripcion VARCHAR(255) NOT NULL);
+	Codigo numeric(18,0) PRIMARY KEY,
+	Descripcion VARCHAR(255) NOT NULL);
 
 CREATE TABLE Especialidades(
-	Especialidad_codigo numeric(18,0) PRIMARY KEY,
-	Especialidad_descripcion VARCHAR(255) NOT NULL,
-	Especialidad_tipo numeric(18,0)
-	FOREIGN KEY (Especialidad_tipo) REFERENCES Tipo_Especialidad (Tipo_codigo));
+	Codigo numeric(18,0) PRIMARY KEY,
+	Descripcion VARCHAR(255) NOT NULL,
+	Tipo numeric(18,0)
+	FOREIGN KEY (Tipo) REFERENCES Tipo_Especialidad (Codigo));
 
 CREATE TABLE Especialidad_Profesional(
 	Especialidad_codigo numeric(18,0),
 	Profesional_id INT,
 	PRIMARY KEY(Especialidad_codigo,Profesional_id),
-	FOREIGN KEY(Especialidad_codigo) REFERENCES Especialidades (Especialidad_codigo),
-	FOREIGN KEY(Profesional_id) REFERENCES Profesionales (Profesional_id));
+	FOREIGN KEY(Especialidad_codigo) REFERENCES Especialidades (Codigo),
+	FOREIGN KEY(Profesional_id) REFERENCES Profesionales (Id));
 
 CREATE TABLE Cancelaciones(
-	Cancelacion_id INT IDENTITY(1,1) PRIMARY KEY,
-	Cancelacion_tipo VARCHAR(30) CHECK (Cancelacion_tipo IN ('Afiliado', 'Profesional')),
-	Cancelacion_detalle VARCHAR(255),
-	Cancelacion_fecha DATETIME);
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Tipo VARCHAR(30) CHECK (Tipo IN ('Afiliado', 'Profesional')),
+	Detalle VARCHAR(400),
+	Fecha DATETIME);
 
 CREATE TABLE Turnos(
-	Turno_id INT IDENTITY(1,1) PRIMARY KEY,
-	Turno_afiliado_id INT NOT NULL,
-	Turno_profesional_id INT NOT NULL,
-	Turno_fecha DATETIME NOT NULL,
-	Turno_especialidad numeric(18,0) NOT NULL,
-	Turno_fecha_llegada DATETIME NULL,
-	Turno_cancelacion INT NULL,
-	FOREIGN KEY(Turno_afiliado_id) REFERENCES Afiliados (Afiliado_id),
-	FOREIGN KEY(Turno_profesional_id) REFERENCES Profesionales (Profesional_id),
-	FOREIGN KEY(Turno_especialidad) REFERENCES Especialidades (Especialidad_codigo),
-	FOREIGN KEY(Turno_cancelacion) REFERENCES Cancelaciones (Cancelacion_id));
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Afiliado_id INT NOT NULL,
+	Profesional_id INT NOT NULL,
+	Fecha DATETIME NOT NULL,
+	Especialidad numeric(18,0) NOT NULL,
+	Fecha_llegada DATETIME NULL,
+	Cancelacion INT NULL,
+	FOREIGN KEY(Afiliado_id) REFERENCES Afiliados (Id),
+	FOREIGN KEY(Profesional_id) REFERENCES Profesionales (Id),
+	FOREIGN KEY(Especialidad) REFERENCES Especialidades (Codigo),
+	FOREIGN KEY(Cancelacion) REFERENCES Cancelaciones (Id));
 
 CREATE TABLE Diagnosticos(
-	Diagnostico_id INT IDENTITY(1,1) PRIMARY KEY,
-	Diagnostico_Af_Id INT NOT NULL,
-	Diagnostico_Pf_Id INT NOT NULL,
-	Diagnostico_Fecha DATETIME NOT NULL,
-	Diagnostico_sintoma VARCHAR(255) NOT NULL,
-	Diagnostico_enfermedad VARCHAR(255),
-	FOREIGN KEY(Diagnostico_Af_Id) REFERENCES Afiliados (Afiliado_id),
-	FOREIGN KEY(Diagnostico_Pf_Id) REFERENCES Profesionales (Profesional_id));
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Af_Id INT NOT NULL,
+	Pf_Id INT NOT NULL,
+	Fecha DATETIME NOT NULL,
+	Sintoma VARCHAR(255) NOT NULL,
+	Enfermedad VARCHAR(255),
+	FOREIGN KEY(Af_Id) REFERENCES Afiliados (Id),
+	FOREIGN KEY(Pf_Id) REFERENCES Profesionales (Id));
 
 CREATE TABLE Bonos_Consultas(
-	Bonos_Consultas_Id INT IDENTITY(1,1) PRIMARY KEY,
-	Bono_Cons_Nro_Consulta INT NULL,
-	Bono_Cons_GrupAf INT NOT NULL,
-	Bono_Cons_Plan numeric (18,0) NOT NULL,
-	Bono_Cons_Af_Uso INT NULL, --Afiliado que lo utilizo
-	Bono_Cons_Fecha_Bono DATETIME,
-	Bono_Cons_Fecha_Imp DATETIME,
-	Bono_Cons_Turno INT NULL,
-	FOREIGN KEY(Bono_Cons_GrupAf) REFERENCES Grupos_Familiares (Grupo_id),
-	FOREIGN KEY(Bono_Cons_Plan) REFERENCES Planes (Plan_codigo),
-	FOREIGN KEY(Bono_Cons_Turno) REFERENCES Turnos (Turno_id),
-	FOREIGN KEY(Bono_Cons_Af_Uso) REFERENCES Afiliados (Afiliado_id));
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Nro_Consulta INT NULL,
+	GrupAf INT NOT NULL,
+	Plan_Uso numeric(18,0) NOT NULL,
+	Af_Uso INT NULL, --Afiliado que lo utilizo
+	Fecha_Bono DATETIME,
+	Fecha_Imp DATETIME,
+	Turno INT NULL,
+	FOREIGN KEY(GrupAf) REFERENCES Grupos_Familiares (Id),
+	FOREIGN KEY(Plan_Uso) REFERENCES Planes (Codigo),
+	FOREIGN KEY(Turno) REFERENCES Turnos (Id),
+	FOREIGN KEY(Af_Uso) REFERENCES Afiliados (Id));
 
 CREATE TABLE Bonos_Farmacia(
-	Bono_Farm_id INT IDENTITY(1,1) PRIMARY KEY,
-	Bono_Farm_Af_Uso INT NULL,
-	Bono_Farm_GrupAf INT NOT NULL,
-	Bono_Farm_Plan_Uso numeric(18,0) NOT NULL,
-	Bono_Farm_Fecha DATETIME,
-	FOREIGN KEY(Bono_Farm_GrupAf) REFERENCES Grupos_Familiares (Grupo_id),
-	FOREIGN KEY(Bono_Farm_Plan_Uso) REFERENCES Planes (Plan_codigo),
-	FOREIGN KEY(Bono_Farm_Af_Uso) REFERENCES Afiliados (Afiliado_id));
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Afiliado_uso INT NULL,
+	Grupo_Afiliado INT NOT NULL,
+	Plan_Uso numeric(18,0) NOT NULL,
+	Fecha DATETIME,
+	FOREIGN KEY(Grupo_Afiliado) REFERENCES Grupos_Familiares (Id),
+	FOREIGN KEY(Plan_Uso) REFERENCES Planes (Codigo),
+	FOREIGN KEY(Afiliado_uso) REFERENCES Afiliados (Id));
 
 CREATE TABLE Agenda_Laboral(
-	Disponibilidad_id INT IDENTITY(1,1),
-	Disponibilidad_profesional INT,
-	Disponibildiad_dia DATETIME,
-	Disponibilidad_desde TIME,
-	Disponibilidad_hasta TIME,
-	Disponibilidad_cancelado INT,
-	FOREIGN KEY (Disponibilidad_profesional) REFERENCES Profesionales (Profesional_id),
-	FOREIGN KEY(Disponibilidad_cancelado) REFERENCES Cancelaciones (Cancelacion_id));
+	Id INT IDENTITY(1,1),
+	Profesional INT,
+	Dia DATETIME,
+	Desde TIME,
+	Hasta TIME,
+	Cancelado INT NULL,
+	FOREIGN KEY (Profesional) REFERENCES Profesionales (Id),
+	FOREIGN KEY(Cancelado) REFERENCES Cancelaciones (Id));
+GO
 
+
+CREATE PROCEDURE BorrarTablas
+AS
+DROP TABLE Agenda_Laboral
+DROP TABLE Bonos_Farmacia
+DROP TABLE Bonos_Consultas
+DROP TABLE Diagnosticos
+DROP TABLE Turnos
+DROP TABLE Cancelaciones
+DROP TABLE Especialidad_Profesional
+DROP TABLE Especialidades
+DROP TABLE Tipo_Especialidad
+DROP TABLE Profesionales
+DROP TABLE Transacciones
+DROP TABLE LogsCambioAfiliados
+DROP TABLE Afiliados
+DROP TABLE Grupos_Familiares
+DROP TABLE Planes
+DROP TABLE Roles_Usuario
+DROP TABLE Usuarios
+DROP TABLE Funciones_Roles
+DROP TABLE Funciones
+DROP TABLE Roles
+GO
+
+CREATE PROCEDURE RegistroAfiliados
+AS
+	SELECT DISTINCT Paciente_Nombre, Paciente_Apellido, Paciente_Dni, Paciente_Direccion, Paciente_Telefono, Paciente_Mail, Paciente_Fecha_Nac, Plan_Med_Codigo
+	INTO #auxiliarPaciente
+	FROM gd_esquema.Maestra
+	DECLARE @PacienteDNI AS numeric(18,0)
+	DECLARE @PacienteApellido AS varchar(255)
+	DECLARE @PacienteDireccion AS varchar(255)
+	DECLARE @PacienteTelefono AS numeric(18,0)
+	DECLARE @PacienteFechaNac AS datetime
+	DECLARE @PacienteMail AS varchar(255)
+	DECLARE @PlanMed AS numeric(18,0)
+	DECLARE @PacienteNombre AS varchar(255)
+	DECLARE CursorPaciente CURSOR FOR SELECT Paciente_Nombre, Paciente_Apellido, Paciente_Dni, Paciente_Direccion, Paciente_Telefono, Paciente_Mail, Paciente_Fecha_Nac, Plan_Med_Codigo FROM #auxiliarPaciente
+	--Abrimos cursor
+	OPEN CursorPaciente
+	FETCH NEXT FROM CursorPaciente INTO @PacienteNombre, @PacienteApellido, @PacienteDNI, @PacienteDireccion, @PacienteTelefono, @PacienteMail, @PacienteFechaNac, @PlanMed
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		INSERT INTO dbo.Grupos_Familiares (Plan_Grupo) VALUES (@PlanMed)
+		INSERT INTO dbo.Afiliados (Numero_de_grupo, Numero_en_el_grupo ,Nombre, Apellido, Tipo_doc, Numero_doc, Direccion, Telefono, Mail, Fecha_nacimiento, Sexo, Estado_civil, Familiares_a_cargo, Esta_activo, Nombre_usuario)
+		VALUES
+		((SELECT TOP 1 Id FROM dbo.Grupos_Familiares ORDER BY Id DESC),01, @PacienteNombre, @PacienteApellido,'DNI', @PacienteDNI, @PacienteDireccion, @PacienteTelefono,@PacienteMail,@PacienteFechaNac,NULL,NULL,NULL,1,NULL)
+		FETCH NEXT FROM CursorPaciente INTO @PacienteNombre, @PacienteApellido, @PacienteDNI, @PacienteDireccion, @PacienteTelefono, @PacienteMail, @PacienteFechaNac, @PlanMed
+		
+	END
+	CLOSE CursorPaciente
+	DEALLOCATE CursorPaciente
+	DROP TABLE #auxiliarPaciente
+GO
+
+
+CREATE PROCEDURE RegistrarPlanes
+AS
+INSERT INTO dbo.Planes (Codigo, Descripcion, Precio_bono_consulta, Precio_bono_farmacia)
+SELECT DISTINCT Plan_Med_Codigo, Plan_Med_Descripcion, Plan_Med_Precio_Bono_Consulta, Plan_Med_Precio_Bono_Farmacia
+FROM gd_esquema.Maestra
+GO
+
+EXEC BorrarTablas
+EXEC CrearTablas
+EXEC RegistrarPlanes
+EXEC RegistroAfiliados
+
+
+	
