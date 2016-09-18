4e4774826951cd147df4c47a33c9ab09fd34b083
Federico Venini <fedevenini@hotmail.com>
2016-09-18 00:12:47 -0300

    Vista Visualización

--- src/ClinicaFrba/Abm Afiliado/Afiliado.cs
+++ src/ClinicaFrba/Abm Afiliado/Afiliado.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +15,28 @@ namespace ClinicaFrba.Abm_Afiliado
         public Afiliado()
         {
             InitializeComponent();
+        }
+
+        private void afiliadosBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.afiliadosBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.gD2C2016DataSet);
+
+        }
+
+        private void Afiliado_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'gD2C2016DataSet.Grupos_Familiares' Puede moverla o quitarla según sea necesario.
+            this.grupos_FamiliaresTableAdapter.Fill(this.gD2C2016DataSet.Grupos_Familiares);
+            // TODO: esta línea de código carga datos en la tabla 'gD2C2016DataSet.Afiliados' Puede moverla o quitarla según sea necesario.
+            this.afiliadosTableAdapter.Fill(this.gD2C2016DataSet.Afiliados);
+
+        }
+
+        private void afiliadosDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
