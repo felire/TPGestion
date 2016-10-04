1cc984f39593135273d6d148c9fb0a293bae1bd6
santipecin <santi_pecin@hotmail.com>
2016-10-04 18:55:20 -0300

    subo clase FranjaCancelda

---
+++ src/ClinicaFrba/UtilConexion/FranjaCancelada.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClinicaFrba.UtilConexion;
+
+namespace ClinicaFrba.UtilConexion
+{
+    class FranjaCancelada
+    {
+        public DateTime desde;
+        public DateTime hasta;
+
+        public FranjaCancelada(DateTime desde, DateTime hasta)
+        {
+            this.desde = desde;
+            this.hasta = hasta;
+        }
+
+        public Boolean fechaFueCancelada(Fecha fecha)
+        {
+            return (fecha.CompareTo(desde) >= 0 && fecha.CompareTo(hasta) <= 0);
+        }
+    }
+}
