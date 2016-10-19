1b53fc89b3b023777f353b86fb84646ba2f9899c
santipecin <santi_pecin@hotmail.com>
2016-10-19 17:54:08 -0300

    arreglo problema en mostrar horas en compra bono te mostraba como opcion la hora a la que se iba el prof ahora te muestra media hora entes nomas, ademas cambio contador por boleano en los loops de la clase HORA

--- src/ClinicaFrba/UtilConexion/Hora.cs
+++ src/ClinicaFrba/UtilConexion/Hora.cs
@@ -19,33 +19,36 @@ namespace ClinicaFrba.UtilConexion
 
         public static List<Hora> obtenerHorasFecha(Fecha dia)
         {
-            int cont = 0;
+            bool horaEnPunto = true;
             List<Hora> lista = new List<Hora>();
+            Hora ultima = null;
             for (int i = dia.horaDesde.Hours; i <= dia.horaHasta.Hours; i++)
             {
-                cont++;
-                if (cont != 2)
+                if (horaEnPunto)
                 {
                     TimeSpan unaHora = new TimeSpan(i, 00, 0);
                     string hora = unaHora.Hours.ToString() + ":" + unaHora.Minutes.ToString() + "0";
                     if (horaValida(dia, unaHora))
                     {
-                        lista.Add(new Hora(unaHora, hora));
+                        ultima = new Hora(unaHora, hora);
+                        lista.Add(ultima);
                     }
+                    horaEnPunto = false;
                 }
                 else
                 {
-                    //REINICIO EL CONTADOR PORQUE ES MEDIA HORA
-                    cont = 0;
                     i--;
                     TimeSpan unaHora = new TimeSpan(i, 30, 0);
                     string hora = unaHora.Hours.ToString() + ":" + unaHora.Minutes.ToString();
                     if (horaValida(dia, unaHora))
                     {
-                        lista.Add(new Hora(unaHora, hora));
+                        ultima = new Hora(unaHora, hora);
+                        lista.Add(ultima);
                     }
+                    horaEnPunto = true;
                 }
             }
+            if(ultima != null) lista.Remove(ultima);
             return lista;
         }
 
@@ -59,25 +62,24 @@ namespace ClinicaFrba.UtilConexion
 
         public static List<Hora> obtenerHorasDia(Dia dia)
         {
-            int cont = 0;
+            bool horaEnPunto = true;
             List<Hora> lista = new List<Hora>();
             for (int i = dia.horaDesde.Hours; i <= dia.horaHasta.Hours; i++)
             {
-                cont++;
-                if (cont != 2)
+                if (horaEnPunto)
                 {
                     TimeSpan unaHora = new TimeSpan(i, 00, 0);
                     string hora = unaHora.Hours.ToString() + ":" + unaHora.Minutes.ToString() + "0";
                     lista.Add(new Hora(unaHora, hora));
+                    horaEnPunto = false;
                 }
                 else
                 {   
-                    //REINICIO EL CONTADOR PORQUE ES MEDIA HORA
-                    cont = 0;
                     i--;
                     TimeSpan unaHora = new TimeSpan(i, 30, 0);
                     string hora = unaHora.Hours.ToString() + ":" + unaHora.Minutes.ToString();
                     lista.Add(new Hora(unaHora, hora));
+                    horaEnPunto = true;
                 }
             }
             return lista;
@@ -102,51 +104,48 @@ namespace ClinicaFrba.UtilConexion
 
         public static List<Hora> ObtenerHorasAceptables(Turno turno)
         {
-            int cont;
             List<Hora> lista = new List<Hora>();
             if (turno.fecha.Minute == 0)
             {
-                cont = 0;
+                bool horaEnPunto = true;
                 for (int i = turno.fecha.TimeOfDay.Hours; i <= turno.fecha.TimeOfDay.Hours + 3; i++)
                 {
-                    cont++;
-                    if (cont != 2)
+                    if (horaEnPunto)
                     {
                         TimeSpan unaHora = new TimeSpan(i, 00, 0);
                         string hora = unaHora.Hours.ToString() + ":" + unaHora.Minutes.ToString() + "0";
                         lista.Add(new Hora(unaHora, hora));
+                        horaEnPunto = false;
                     }
                     else
                     {
-                        //REINICIO EL CONTADOR PORQUE ES MEDIA HORA
-                        cont = 0;
                         i--;
                         TimeSpan unaHora = new TimeSpan(i, 30, 0);
                         string hora = unaHora.Hours.ToString() + ":" + unaHora.Minutes.ToString();
                         lista.Add(new Hora(unaHora, hora));
+                        horaEnPunto = true;
                     }
                 }
             }
             else
             {
-                cont = 1;
+                bool horaEnPunto = false;
                 for (int i = turno.fecha.TimeOfDay.Hours + 1; i <= turno.fecha.TimeOfDay.Hours + 3; i++)
                 {
-                    cont++;
-                    if (cont != 2)
+                    if (horaEnPunto)
                     {
                         TimeSpan unaHora = new TimeSpan(i, 00, 0);
                         string hora = unaHora.Hours.ToString() + ":" + unaHora.Minutes.ToString() + "0";
                         lista.Add(new Hora(unaHora, hora));
+                        horaEnPunto = false;
                     }
                     else
                     {
-                        //REINICIO EL CONTADOR PORQUE ES MEDIA HORA
-                        cont = 0;
                         i--;
                         TimeSpan unaHora = new TimeSpan(i, 30, 0);
                         string hora = unaHora.Hours.ToString() + ":" + unaHora.Minutes.ToString();
                         lista.Add(new Hora(unaHora, hora));
+                        horaEnPunto = true;
                     }
                 }
             }
